f87040d48e8a911dd749d890618f0b5c966fd656
jonathan <jonaf2103@gmail.com>
2021-08-12 18:04:15 -0300

    Removing unrelated changes

---
+++ dist/utils.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="@openeth/truffle-typings" />
+import { AbiItem } from 'web3-utils';
+import { RelayingServicesAddresses } from './interfaces';
+import { Contract } from 'web3-eth-contract';
+import { EnvelopingConfig } from '@rsksmart/rif-relay-common';
+export declare function getAbiItem(contractAbi: AbiItem[], itemName: string): AbiItem;
+export declare function addressHasCode(web3: Web3, smartWalletAddress: string): Promise<boolean>;
+export declare function getContract(web3: Web3, contractAbi: AbiItem[], contractAddress: string): Contract;
+export declare function getContractAddresses(chainId: number): RelayingServicesAddresses;
+export declare function mergeConfiguration(inputConfig: any, mergeConfig: any): Partial<EnvelopingConfig>;
